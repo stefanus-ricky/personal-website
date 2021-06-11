d163faed4253133c6bb27d323defafbe286612b0
srickyr <srickyriady@gmail.com>
2021-06-11 11:02:53 +0700

    cleaning unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/:contentName"  component={Page}>
+
+          </Route>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
